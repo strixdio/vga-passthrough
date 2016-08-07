37cbccdc704d35261313d5c120f21da363b7ce4b
strixdio <strixd.io@protonmail.ch>
2016-08-07 16:51:08 -0400

    reworked

--- vfio-setup.sh
+++ vfio-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
+uselist=""
+gpulist=""
+
 function print_warning ()
 {
 	clear
@@ -7,6 +11,102 @@ function print_warning ()
 	echo "Press \"Enter\" to continue, or ctrl+c to quit."
 	echo
 	read
+}
+
+function tidy_string()
+{
+        echo $(sed -e 's/  / /g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< $@ | tr '\n' ' ')
+}
+
+function get_gpus()
+{
+        gpulist=$(lspci -m | grep VGA | awk -F' ' '{ print $1 }')
+}
+
+function select_gpus()
+{
+        echo
+        echo "Select GPU(s) you wish to use for vga-passthrough. 'd' for done."
+        select gpu in $gpulist
+        do
+                case $gpu in
+                "$QUIT")
+                        break
+                ;;
+                *)
+                        if [[ ! $uselist == *$gpu* ]]
+                        then
+                                uselist="$uselist $gpu"
+                        else
+                                uselist=`sed "s/$gpu//g" <<< $uselist`
+                        fi
+                        uselist=$(tidy_string $uselist)
+                        echo "Selected: $uselist"
+                ;;
+                esac
+        done
+        print_uselist
+}
+
+function print_uselist()
+{
+	echo "Using:"
+        for i in $uselist
+        do
+                find=$(awk -F'.' '{ print $1 }' <<< $i)
+                echo "$(lspci -m | grep $find)"
+        done
+}
+
+function print_gpulist()
+{
+        echo "GPU's and their audio devices (if applicable) found:"
+        echo
+        for i in $gpulist
+        do
+                find=$(awk -F'.' '{ print $1 }' <<< $i)
+                echo "$(lspci -m | grep $find)"
+        done
+}
+
+function gen_wrapper()
+{
+        cmd_list=""
+        cmd_sed=" | sed 's|aAaA|aAaA,x-vga=on|g'"
+        for i in $uselist
+        do
+                cmd_replace=$(sed "s/aAaA/$i/g" <<< $cmd_sed)
+                cmd_list="$cmd_list $cmd_replace"
+        done
+	if [[ -e /usr/bin/qemu-kvm ]]
+	then
+        	cmd="exec /usr/bin/qemu-kvm \`echo \"\$@\"$cmd_list\`"
+	else
+		cmd="exec /usr/bin/qemu-system-x86_64 \`echo \"\$@\"$cmd_list\`"
+	fi
+	echo "#!/bin/bash" > ./qemu-kvm.vga.gen
+	echo "" >> ./qemu-kvm.vga.gen
+        echo $cmd >> ./qemu-kvm.vga.gen
+}
+
+function gen_override()
+{
+        for i in $uselist
+        do
+                find=$(awk -F'.' '{ print $1 }' <<< $i)
+                cmd_devs=$(tidy_string "$cmd_devs $(lspci -m | grep $find | awk -F' ' '{ print $1}')")
+        done
+	
+	echo "#!/bin/sh" > ./vfio-pci-override-vga.sh.gen
+	echo "" >> ./vfio-pci-override-vga.sh.gen
+	echo "DEVS=\"aAaA\"" | sed "s/aAaA/$cmd_devs/g" >> ./vfio-pci-override-vga.sh.gen
+	echo "" >> ./vfio-pci-override-vga.sh.gen
+	echo "for DEV in \$DEVS; do" >> ./vfio-pci-override-vga.sh.gen
+    	echo "	echo \"vfio-pci\" > /sys/bus/pci/devices/\$DEV/driver_override" >> ./vfio-pci-override-vga.sh.gen
+	echo "done" >> ./vfio-pci-override-vga.sh.gen
+	echo "" >> ./vfio-pci-override-vga.sh.gen
+	echo "modprobe -i vfio-pci" >> ./vfio-pci-override-vga.sh.gen
+        #sed -i "s/aAaA/$cmd_devs/g" ./vfio-pci-override-vga.sh.gen > ./vfio-pci-override-vga.sh.gen
 }
 
 function get_cpu_params ()
@@ -131,7 +231,7 @@ function update_modprobe ()
 	then
 	        cp /etc/modprobe.d/vfio.conf /etc/modprobe.d/vfio.conf.orig
 	fi
-	cp -f ./vfio-pci-override-vga.sh /sbin/
+	cp -f ./vfio-pci-override-vga.sh.gen /sbin/vfio-pci-override-vga.sh
 	chmod 775 /sbin/vfio-pci-override-vga.sh
 	echo 'install vfio-pci /sbin/vfio-pci-override-vga.sh' > /etc/modprobe.d/vfio.conf
 }
@@ -152,6 +252,13 @@ function main ()
 	print_warning
 	backup_grub
 	
+        get_gpus
+        print_gpulist
+        select_gpus
+        gen_wrapper
+        gen_override
+	echo
+
 	# Figure out kernel params
 	get_cpu_params
 	if [ ! "$params" == "" ]
@@ -169,8 +276,8 @@ function main ()
 	update_modprobe
 	grub2-mkconfig -o /boot/grub2/grub.cfg
 	update_dracut
-	cp -f ./qemu-kvm.vga /usr/bin/qemu-kvm.vga
-
+	cp -f ./qemu-kvm.vga.gen /usr/bin/qemu-kvm.vga
+	rm -rf ./qemu-kvm.vga.gen vfio-pci-override-vga.sh.gen
 	echo "Done. To use vga-passthrough with virt-manager, "virsh edit" to modify <emulator> contents to /usr/bin/qemu-kvm.vga"
 }
 
